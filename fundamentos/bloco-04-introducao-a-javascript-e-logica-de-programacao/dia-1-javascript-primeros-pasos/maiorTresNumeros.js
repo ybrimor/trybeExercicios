8fea1f06139bec6c693eb410cb9af16b8019ae8a
Yuny Mar Briceño Moreno <ybrimor@icloud.com>
2021-11-26 20:37:57 -0400

    cria programa que retorna o maior de três números

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/maiorTresNumeros.js
@@ -0,0 +1,10 @@
+const num1 = 250;
+const num2 = 73;
+const num3 = 17;
+if (num1 > num2 && num1>num3) {
+    console.log(num1);
+} else if (num2 > num1 && num2>num3) {
+    console.log(num2);
+} else {
+    console.log(num3);
+}
