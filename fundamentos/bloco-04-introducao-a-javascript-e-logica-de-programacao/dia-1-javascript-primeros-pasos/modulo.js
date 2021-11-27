e6ea279bd7e880a399ffe6056a4f55a8b65b84a7
Yuny Mar Briceño Moreno <ybrimor@icloud.com>
2021-11-26 20:12:34 -0400

    modifica o valor da constante 'a'

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/modulo.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/modulo.js
@@ -1,4 +1,4 @@
-const a = 20;
+const a = 45;
 const b = 15;
 let modulo = a % b;
 console.log(modulo);
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/subtracao.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/subtracao.js
@@ -1,4 +1,4 @@
-const a = 20;
+const a = 45;
 const b = 15;
 let subtracao = a - b;
 console.log(subtracao);
