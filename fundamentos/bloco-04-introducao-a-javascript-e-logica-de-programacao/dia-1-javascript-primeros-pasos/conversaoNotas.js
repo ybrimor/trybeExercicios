de5c5f0e22c4058a4dfa64188ff13db8fdc9d644
Yuny Mar Briceño Moreno <ybrimor@icloud.com>
2021-11-27 11:33:40 -0400

    cria programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeros-pasos/conversaoNotas.js
@@ -0,0 +1,16 @@
+let notaPorcentagem = 99;
+let notaConceito;
+if (notaPorcentagem >= 90){
+    console.log('Nota em conceito: A');
+} else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
+    console.log('Nota em conceito: B');
+} else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
+    console.log('Nota em conceito: C');
+}else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
+    console.log('Nota em conceito: D');
+}else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
+    console.log('Nota em conceito: E');
+}else  {
+    console.log('Nota em conceito: F');
+}
+
