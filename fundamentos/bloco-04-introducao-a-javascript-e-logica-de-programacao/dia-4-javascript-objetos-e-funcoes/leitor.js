b4d60f4b1f3f315672fd043143669630e87e08d8
Yuny Mar Briceño Moreno <ybrimor@icloud.com>
2021-12-02 00:13:55 -0400

    adiciona um elemento na chave livrosFavoritos e faz um sonsole log

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/leitor.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/leitor.js
@@ -12,3 +12,10 @@ let leitor = {
   };
   
   console.log('O livro favorito de', leitor.nome + leitor.sobrenome, 'se chama "', leitor['livrosFavoritos'][0]['titulo'],'"');
+  
+  leitor['livrosFavoritos'][1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+  console.log(leitor.nome,'tem', leitor.livrosFavoritos.length, 'livros favoritos');
