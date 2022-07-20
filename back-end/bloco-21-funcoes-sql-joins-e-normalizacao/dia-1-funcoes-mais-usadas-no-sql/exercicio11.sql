ba5836cc620c20a90bfebff8a04ac790a5d2e230
Yuny Mar Briceño Moreno <ybrimor@icloud.com>
2022-07-19 20:02:17 -0400

    feat: Implementa uma query que atualiza a coluna phone_number, de modo que todos os telefones iniciados por 515 agora devem iniciar com 777

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercicio11.sql
@@ -0,0 +1,9 @@
+# EXERCICIO 11:  Escreva uma query que atualize a coluna phone_number,
+# de modo que todos os telefones iniciados por 515 agora devem iniciar com 777
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE hr.employees
+SET phone_number = REPLACE(phone_number, '515', '777')
+WHERE phone_number LIKE '515%';
+
+SET SQL_SAFE_UPDATES = 1;
